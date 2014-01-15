60633a7fb4993dda72a51f04dfa155d99424ad67
SidC <sid.chaudhary@me.com>
2014-01-15 19:39:49 +0530

    Created todo page
    Added Todo page to the angular-seed project.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,4 +8,14 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', [function() {
 
-  }]);
+  }])
+  .controller('mainCtrl', function ($scope) {
+    $scope.todos = ["First Item", "Second Item", "Third Item"];
+    $scope.addTodo = function () {
+    	$scope.todos.push($scope.todo);
+    	$scope.todo = "";
+    }
+    $scope.removeTodo = function (index) {
+    	$scope.todos.splice(index, 1);
+    }
+  });
